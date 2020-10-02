11b6a2a0bea4fd283c4ad8c534f4e24cbe30712a
InvisibleMax <37413129+InvisibleMax@users.noreply.github.com>
2020-10-02 10:46:50 +0530

    max heap

---
+++ max heap using stl.cpp
@@ -0,0 +1,25 @@
+
+//https://www.geeksforgeeks.org/implement-min-heap-using-stl/
+#include <bits/stdc++.h>
+using namespace std;
+
+// Driver code
+int main ()
+{
+    // Creates a max heap
+    priority_queue <int> pq;
+    pq.push(5);
+    pq.push(1);
+    pq.push(10);
+    pq.push(30);
+    pq.push(20);
+
+    // One by one extract items from max heap
+    while (pq.empty() == false)
+    {
+        cout << pq.top() << " ";
+        pq.pop();
+    }
+
+    return 0;
+}
